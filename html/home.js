a3d79d3263f7ef1a2c729c7eb291c8ebd74e0db6
itsshri <122509758+itsshri@users.noreply.github.com>
2023-02-20 15:33:55 +0530

    Add files via upload

---
+++ html/home.js
@@ -0,0 +1,7 @@
+function play()
+{
+    var audio =document.getElementById("audio");
+    audio.play();
+}
+<input type="button" value="Play" onclick="play()">
+<audio autoplay id="audio" src="C:\Users\Shrijith\Downloads\Thenmozhi-MassTamilan.dev.mp3"></audio>
---
+++ html/redirect.js
@@ -0,0 +1,4 @@
+
+function myFunction() {
+    alert("Premium Access Successful Experience it From Now!");
+  }
